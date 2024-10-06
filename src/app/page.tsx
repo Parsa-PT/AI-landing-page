d93e7da4c09de80a4716eecd4352151f45d501e3
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-10-06 22:13:02 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Features } from "@/sections/Features";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { Testimonials } from "@/sections/Testimonials";
 
 export default function Home() {
   return <div>
     <Header/>
     <Hero/>
+    <LogoTicker/>
+    <Features/>
+    <Testimonials/>
   </div>;
 }
