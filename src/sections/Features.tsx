800b569f08cce7545a82c8cd68f58456a3de4cd9
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-10-06 22:11:38 +0330

    Update Features.tsx

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,8 @@
+'use client'
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import Image from "next/image";
+import Productimg from '@/assets/product-image.png'
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +31,34 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section className=" py-20 md:py-24">
+      <div className="container">
+          <h2 className="text-5xl md:text-6xl text-white font-medium text-center tracking-tighter">
+          Elevate your SEO efforts.
+          </h2>
+          <p className=" md:text-xl max-w-2xl  mx-auto text-white/70  text-lg tracking-tighter text-center mt-5">
+            From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
+          </p>
+          <div className=" mt-10 flex flex-col lg:flex-row lg:gap-3 lg:justify-between ">
+          {tabs.map((item)=>(
+            <div className=" border border-white/15 flex p-2.5  lg:flex-1 rounded-xl gap-2.5 items-center mb-3" key={item.title}>
+                <div className=" h-12 w-12 border border-white/15 rounded-lg flex items-center justify-center">
+                <DotLottiePlayer src={item.icon} autoplay className=" h-5 w-5" />
+                </div>
+                <div className=" text-white font-medium">{item.title}</div>
+                {item.isNew && (
+                  <div className=" text-black font-semibold bg-[#8c44ff] rounded-full px-2 text-xs py-0.5">new</div>
+                )}
+            </div>
+          ))}
+          </div>
+
+          <div className=" border  border-white/20 p-2.5 mt-3 rounded-lg">
+          <div style={{backgroundImage:`url(${Productimg.src})`}} className="  aspect-video bg-cover  "></div>
+            {/* <Image src={Productimg} alt="pro" /> */}
+          </div>
+      </div>
+    </section>
+  );
 };
