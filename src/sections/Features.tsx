8829ddfc7eb0ccb708462a87ef8fea14cfc5d54a
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-10-08 22:48:19 +0330

    Update Features.tsx

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,7 +1,9 @@
 'use client'
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import Productimg from '@/assets/product-image.png'
+import { useEffect, useRef } from "react";
+import { useMotionTemplate, useMotionValue , motion, animate } from "framer-motion";
 
 const tabs = [
   {
@@ -30,7 +32,60 @@ const tabs = [
   },
 ];
 
+
+const FeaturesTab =({item}:any)=>{
+
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null)
+
+  const handletohover = ()=>{
+    if(dotLottieRef.current === null) return
+    dotLottieRef.current.seek(0)
+    dotLottieRef.current.play()
+  }
+
+  const xPercentage = useMotionValue(0)
+  const yPercentage = useMotionValue(0)
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%,black,transparent)`
+
+  useEffect(()=>{
+     animate(xPercentage , [0,100,100,0,0] , {
+      duration:4,
+      repeat:Infinity,
+      ease:'linear',
+      repeatType:'loop'
+     })
+     animate(yPercentage , [0,0,100,100,0] , {
+      duration:4,
+      repeat:Infinity,
+      ease:'linear',
+      repeatType:'loop'
+     })
+  },[xPercentage , yPercentage])
+  
+
+  return (
+    <div  onMouseEnter={handletohover} className=" border relative border-white/15 flex p-2.5  lg:flex-1 rounded-xl gap-2.5 items-center mb-3" key={item.title}>
+      <motion.div style={{maskImage}} className=" absolute inset-0 border border-[#A369FF]   -m-px rounded-xl [mask-image:] "></motion.div>
+                <div className=" h-12 w-12 border border-white/15  rounded-lg flex items-center justify-center">
+                <DotLottiePlayer ref={dotLottieRef} src={item.icon}  className=" h-5 w-5" />
+                </div>
+                <div className=" text-white font-medium">{item.title}</div>
+                {item.isNew && (
+                  <div className=" text-black font-semibold bg-[#8c44ff] rounded-full px-2 text-xs py-0.5">new</div>
+                )}
+        </div>
+  )
+}
+
 export const Features = () => {
+
+  const backgroundx = useMotionValue(tabs[0].backgroundPositionX)
+  const backgroundY = useMotionValue(tabs[0].backgroundPositionY)
+  const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX)
+
+  const backgroundpositionX = useMotionTemplate`${backgroundx}% ${backgroundY}%`
+  const backgroundpositionSizeX = useMotionTemplate`${backgroundSizeX}% auto`
+
   return (
     <section className=" py-20 md:py-24">
       <div className="container">
@@ -42,21 +97,13 @@ export const Features = () => {
           </p>
           <div className=" mt-10 flex flex-col lg:flex-row lg:gap-3 lg:justify-between ">
           {tabs.map((item)=>(
-            <div className=" border border-white/15 flex p-2.5  lg:flex-1 rounded-xl gap-2.5 items-center mb-3" key={item.title}>
-                <div className=" h-12 w-12 border border-white/15 rounded-lg flex items-center justify-center">
-                <DotLottiePlayer src={item.icon} autoplay className=" h-5 w-5" />
-                </div>
-                <div className=" text-white font-medium">{item.title}</div>
-                {item.isNew && (
-                  <div className=" text-black font-semibold bg-[#8c44ff] rounded-full px-2 text-xs py-0.5">new</div>
-                )}
-            </div>
+              <FeaturesTab key={item} item={item}/>
           ))}
           </div>
 
           <div className=" border  border-white/20 p-2.5 mt-3 rounded-lg">
-          <div style={{backgroundImage:`url(${Productimg.src})`}} className="  aspect-video bg-cover  "></div>
-            {/* <Image src={Productimg} alt="pro" /> */}
+          <motion.div style={{backgroundImage:`url(${Productimg.src})` }} className="  aspect-video bg-cover  "></motion.div>
+            
           </div>
       </div>
     </section>
